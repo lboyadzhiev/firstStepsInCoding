7ff04565221c229a574e25483d818cc25561f7dd
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-10-14 09:36:37 +0300

    Initial Commit: programming basic problems.

---
+++ src/Expression.java
@@ -0,0 +1,5 @@
+public class Expression {
+    public static void main(String[] args) {
+        System.out.println((3522 + 52352) * 23 - (2336 * 501 + 23432 - 6743) *  3);
+    }
+}
---
+++ src/helloSoftUni.java
@@ -0,0 +1,5 @@
+public class helloSoftUni {
+    public static void main(String[] args) {
+        System.out.println("Hello Soft Uni");
+    }
+}
